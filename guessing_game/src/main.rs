75ba7bff2ed4a64271412fbb52c57b2810d86755
Jinwoo An <jinwoo.gus.an@gmail.com>
2019-11-12 17:45:06 +0900

    CH02 11/12

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::io; // standard::i/o library
+             // 'io' is not in std::prelude, which is brought by default.
 
 fn main() {
     println!("Guss the number!");
@@ -6,9 +7,13 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
+    // new(): a static method of type 'String'
+    // Variables are 'immuatable' by default.
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
+    // without 'use std::io', this would be std::io::stdin
+    // which returns an instance of std::io::Stdin.
 
     println!("You guessed: {}", guess);
 }
